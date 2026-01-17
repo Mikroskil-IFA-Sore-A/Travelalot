eb1258321b7226035e29abcc81ad741399ec2fbc
Aelberth Cheong <aelbeth.cheong@outlook.com>
2026-01-17 21:02:14 +0700

    feat: add a bunch of path alias & adding lucide-react and three.js as dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@data": path.resolve(__dirname, "src/data"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@layouts": path.resolve(__dirname, "src/layouts"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+        },
+    },
 })
+
+
