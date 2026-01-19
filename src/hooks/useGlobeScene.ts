3776dbc20cb1174ceca496307d6082c90e538556
Aelberth Cheong <aelbeth.cheong@outlook.com>
2026-01-20 01:00:54 +0700

    fix: refactor 3dscene

--- src/hooks/useGlobeScene.ts
+++ src/hooks/useGlobeScene.ts
@@ -3,10 +3,10 @@ import * as THREE from 'three'; // three.js (i.e. library untuk 3D)
 
 // Literally ini semua, aku dapat dari 'https://threejs.org/docs'
 // Rencana-nya menggunakan React three fiber jg, tapi ini cukup simple -- so, i digress
-export default function(containerRef: RefObject<HTMLDivElement>, onReady?: () => void): void {
+export default function(containerRef: RefObject<HTMLDivElement | null>): void {
     useEffect(() => {
         const container = containerRef.current;
-        if (!containerRef.current) // tunggu div exit
+        if (!container) // tunggu div exit
             return;
 
         const scene = new THREE.Scene();
@@ -43,11 +43,6 @@ export default function(containerRef: RefObject<HTMLDivElement>, onReady?: () =>
         const frame = () => {
             group.rotation.y += 0.25 * clock.getDelta(); // idk why, increment-ing y malah gerakin secara horizontal
             renderer.render(scene, camera);
-
-            if (onReady) {
-                onReady();
-                onReady = undefined;
-            }
         };
 
         renderer.setAnimationLoop(frame); // mirip 'requestAnimationFrame()'?
